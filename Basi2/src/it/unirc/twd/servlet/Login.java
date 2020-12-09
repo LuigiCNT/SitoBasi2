7c66cb0a6fdc5f8bb750362e03f4044f1dc1ecdc
giuseppe-barone <45794194+giuseppe-barone@users.noreply.github.com>
2020-12-09 18:26:58 +0100

    servlet login.java

---
+++ Basi2/src/it/unirc/twd/servlet/Login.java
@@ -0,0 +1,60 @@
+package it.unirc.twd.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import it.unirc.twd.beans.Utente;
+import it.unirc.twd.beans.UtenteDAO;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private UtenteDAO uDAO;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        Utente utente = new Utente();
+        utente.setUsername(username);
+        utente.setPassword(password);
+        
+        if (uDAO.LoginUtente(utente)) {
+		    HttpSession session = request.getSession();
+		    // session.setAttribute("username",username);
+		    response.sendRedirect("LoginSuccesso.jsp");
+		} else {
+		    HttpSession session = request.getSession();
+		    //session.setAttribute("user", username);
+		    response.sendRedirect("Login.jsp");
+		}
+	}
+
+}
